f8d02b9c87505592913259fbeccb07080c602317
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2025-09-12 09:04:20 +0200

    Fix indentation for identifier parameter in graphband.py

--- laufband/graphband.py
+++ laufband/graphband.py
@@ -99,7 +99,7 @@ class Graphband(t.Generic[TaskTypeVar]):
         *,
         lock: Lock = Lock("graphband.lock"),
         db: str = "sqlite:///graphband.sqlite",
-        identifier: str | t.Callable = os.getenv("LAUFBAND_IDENTIFIER") or (lambda: f"{socket.gethostname()}:{os.getpid()}")
+        identifier: str | t.Callable = os.getenv("LAUFBAND_IDENTIFIER") or (lambda: f"{socket.gethostname()}:{os.getpid()}"),
         failure_policy: t.Literal["continue", "stop"] = os.getenv(
             "LAUFBAND_FAILURE_POLICY", "continue"
         ),
